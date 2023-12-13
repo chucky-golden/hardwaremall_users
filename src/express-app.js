022d55414ef48d5664948dd7af9aa822fba45006
chucky-golden <chukschibyke92@gmail.com>
2023-12-13 18:32:38 +0100

    new changes

--- src/express-app.js
+++ src/express-app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const multer = require('multer');
-const bodyParser = require('body-parser');
 const basicRoutes = require('./routes/basicRoutes')
 
 module.exports = async (app) => {
@@ -9,12 +7,6 @@ module.exports = async (app) => {
 
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
-
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({ extended: true }));
-
-    const upload = multer();
-    app.use(upload.array());
 
     app.use('', basicRoutes)
     
